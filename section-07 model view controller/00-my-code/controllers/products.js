86a83b1a5df38bd3d84236f9a20e68dc4e8a58e1
Baber Arjumand <baber.arjumand@gmail.com>
2022-04-17 17:38:46 +0500

    finished section 7

---
+++ section-07 model view controller/00-my-code/controllers/products.js
@@ -0,0 +1,41 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  //   products.push({ title: req.body.title });
+  const newProduct = new Product(req.body.title);
+  newProduct.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+//   const products = Product.fetchAll();
+//   res.render("shop", {
+//     prods: products,
+//     pageTitle: "Shop",
+//     path: "/",
+//     hasProducts: products.length > 0,
+//     activeShop: true,
+//     productCSS: true,
+//   });
+
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
+  });
+};
---
+++ section-07 model view controller/00-my-code/routes/shop.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { getProducts } = require("../controllers/products");
+
+const router = express.Router();
+
+router.get("/", getProducts);
+
+module.exports = router;
